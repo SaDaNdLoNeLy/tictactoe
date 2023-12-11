df86641b4543f44c95d72e27fcac67c0e07426d2
Nguyen Viet Hung <hunglo810@hunglo.hunglo810>
2023-12-11 23:52:21 +0700

    remove push error

---
+++ src/server/server.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int count(int a, int b) {
+    int result = a+b;
+    return result;
+}
+
+void test(char *buff){
+    std::cout << buff << std::endl;
+}
+
+int hello(int main){
+    return main*2;
+}
